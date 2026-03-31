df050f13bce0b6fc599713de050098dbf03df1ec
Sam <doansamquoc@gmail.com>
2026-04-01 02:29:26 +0700

    Initial commit

---
+++ src/main/java/com/sam/mini_cinema/MiniCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.sam.mini_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCinemaApplication.class, args);
+	}
+
+}
